5149abd8b1d91ff24d50e53f4b5d955f15a75b07
Phil Bowsher <phil@rstudio.comgit config --global user.email phil@rstudio.com>
2020-10-27 15:42:29 +0000

    update torn

--- Examples/1_Visualizations/1_htmlwidgets_tornadoes.R
+++ Examples/1_Visualizations/1_htmlwidgets_tornadoes.R
@@ -16,13 +16,13 @@ library(tidyverse)
 
 Torn <- read_csv("~/Foundation-of-the-R-Workflow-workshop-2019-09-06/Examples/data/torn/Torn3.csv")
 
-Torn1 <- Torn[ which((Torn$st=='IL' | Torn$st=='IN') & Torn$yr== 2017), ]
+Torn1 <- Torn[ which((Torn$st=='ME' | Torn$st=='NH') & Torn$yr== 2017), ]
 
 View(Torn1)
 
 library("leaflet") 
 
-leaflet() %>% addTiles() %>% setView(-87.891563, 42.302101, zoom = 9) %>% 
+leaflet() %>% addTiles() %>% setView(-70.275740, 43.653360, zoom = 9) %>% 
   
   addMarkers(data = Torn1, lat = ~ slat, lng = ~ slon, popup = Torn1$date)
 
